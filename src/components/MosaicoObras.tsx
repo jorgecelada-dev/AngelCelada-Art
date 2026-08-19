ed7aadd524f8f1625c46d88823732571b66837ea
Jorge <jorgeceladaa2@gmail.com>
2026-08-19 16:32:17 +0200

    mosaico

--- src/components/MosaicoObras.tsx
+++ src/components/MosaicoObras.tsx
@@ -54,7 +54,7 @@ export default function MosaicoObras({
   }
 
   const colsClase = compacto
-    ? "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
+    ? "grid-cols-1 sm:grid-cols-2 lg:grid-cols-4"
     : "grid-cols-2 sm:grid-cols-3 lg:grid-cols-4";
 
   return (
